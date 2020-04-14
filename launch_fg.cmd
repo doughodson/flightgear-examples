ae30e731b43027211d747ff1b35abb1b7b64e217
Doug Hodson <doug@sidechannel.net>
2020-04-13 20:16:45 -0400

    update

---
+++ launch_fg.cmd
@@ -0,0 +1,4 @@
+rem change working directory to location of binary
+cd e:\apps\flightgear_v2019.1.1\bin
+rem launch FlightGear
+e:\apps\flightgear_v2019.1.1\bin\fgfs.exe --aircraft=c172p --disable-random-objects --fdm=external --vc=0 --bpp=32 --timeofday=noon --native-fdm=socket,in,1,,5500,udp
--- net_fdm/main.cpp
+++ net_fdm/main.cpp
@@ -3,6 +3,7 @@
 
 #include <ctime>
 #include <iostream>
+#include <string>
 
 #include "net_fdm.hxx"
 
@@ -24,11 +25,10 @@ SOCKET sendSocket = -1;
 struct sockaddr_in sendAddr;
 
 // IP and port where FG is listening
-const char* fg_ip = "127.0.0.1";
+const std::string fg_ip{"127.0.0.1"};
 const int fg_port{5500};
 
-// update period.  controls how often updates are
-// sent to FG.  in seconds.
+// period to update fg (in milliseconds)
 const int update_period{1000};
 
 void run();
@@ -39,7 +39,7 @@ int main(int argc, char** argv) {
       memset(&sendAddr,0,sizeof(sendAddr));
       sendAddr.sin_family = AF_INET;
       sendAddr.sin_port = htons(fg_port);
-      sendAddr.sin_addr.S_un.S_addr = inet_addr(fg_ip);
+      sendAddr.sin_addr.S_un.S_addr = inet_addr(fg_ip.c_str());
       sendSocket = socket(AF_INET,SOCK_DGRAM,0);
       if (sendSocket != INVALID_SOCKET) {
          run();
@@ -76,9 +76,9 @@ void run() {
       fdm.longitude = htond(static_cast<double>(longitude * D2R));
       fdm.altitude  = htond(static_cast<double>(altitude));
 
-      fdm.phi = htonf(static_cast<float>(roll * D2R));
+      fdm.phi   = htonf(static_cast<float>(roll * D2R));
       fdm.theta = htonf(static_cast<float>(pitch * D2R));
-      fdm.psi = htonf(static_cast<float>(yaw * D2R));
+      fdm.psi   = htonf(static_cast<float>(yaw * D2R));
 
       fdm.num_engines = htonl(1);
 
@@ -92,14 +92,12 @@ void run() {
 
       fdm.visibility = htonf(visibility);
 
-      sendto(sendSocket,(char *)&fdm,sizeof(fdm),0,(struct sockaddr *)&sendAddr,sizeof(sendAddr));
+      sendto(sendSocket, (char*)&fdm, sizeof(fdm), 0, (struct sockaddr*)&sendAddr, sizeof(sendAddr));
 
-      static bool flag{true};
-      if (flag) {
-         roll += 5.0;
-      } else {
-         roll -= 5.0;
+      roll += 5.0;      // increase roll in 5 degree increments
+      if (roll > 20.0) {
+         roll = 0.0;
       }
-      flag = !flag;
+      std::cout << "Roll: " << roll << std::endl;
    }
 }
