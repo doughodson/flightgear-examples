27688eb831f4525d29d39ca7b4fce85d9a8d5f6e
Doug Hodson <doug@sidechannel.net>
2020-08-06 20:06:31 -0400

    updated flightgear launch script

--- launch_fg.cmd
+++ launch_fg.cmd
@@ -1,2 +1,2 @@
 rem launch FlightGear
-e:\apps\flightgear_v2018.3.5\bin\fgfs.exe --aircraft=ufo --disable-panel --disable-random-objects --fdm=null --vc=0 --timeofday=noon --native-fdm=socket,in,30,,5500,udp
+e:\apps\flightgear_v2018.3.5\bin\fgfs.exe --aircraft=ufo --disable-panel --enable-hud --disable-random-objects --fdm=null --vc=0 --timeofday=noon --native-fdm=socket,in,30,,5500,udp
